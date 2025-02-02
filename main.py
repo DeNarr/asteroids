a45dfe776a9c9c1e86c8afbb9a76ca1215927226
DeNarr <rjsrz9@gmail.com>
2025-02-02 10:51:23 -0600

    Added player file to start drawing

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
+    #Primary game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (0, 0, 0))
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
 
 
